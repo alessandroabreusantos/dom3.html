987d1af3aaf54f6d4a704d882c5d6dac1790efa5
alessandroabreusantos <abreualessandro77@gmail.com>
2025-05-15 08:39:41 -0300

    first commit

---
+++ dom.js
@@ -0,0 +1,9 @@
+//document.getElementById('exemplo') /*pegando o ID*/
+//document.getElementsByClassName('lista')/*pegando a classe*/
+//document.getElementsByTagName('button')/*pegando a tag*/
+
+
+/*queryselector pega tudo*/
+document.querySelector('#exemplo').innertext="hoje e terça-feira"
+document.querySelector('button').innerHTML="<h1>amanha tem jogo</h1>"
+document.querySelector('lista').textcontent="amanha e quarta"
---
+++ dom3.js
@@ -0,0 +1,3 @@
+document.querySelector('button').innerhtml="<h1>borat <h1>"
+document.querySelector('button').innerHTML="<h1>fatima<h!>"
+document.querySelector('button').innerHTML="<h1>ace combat<h1>"
